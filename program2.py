cbd6210d955cfc0f9d597d8b0ba489c57d35e52d
varshithchowdary <varshithlion@gmail.com>
2024-11-11 13:36:04 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,19 +1,5 @@
-def decode_message(s: str, p: str) -> bool:
+def decode_message( s: str, p: str) -> bool:
 
-    dp = [[False] * (len(s) + 1) for _ in range(len(p) + 1)]
-    dp[0][0] = True
-
-    for i in range(1, len(p) + 1):
-        if p[i - 1] == '*':
-            dp[i][0] = dp[i - 1][0]
-        else:
-            break  
-
-    for i in range(1, len(p) + 1):
-        for j in range(1, len(s) + 1):
-            if p[i - 1] == '*':
-
-                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
-            elif p[i - 1] == '?' or p[i - 1] == s[j - 1]:
-
-                dp[i][j] = dp[i - 1][j - 1]
+# write your code here
+  
+        return False
