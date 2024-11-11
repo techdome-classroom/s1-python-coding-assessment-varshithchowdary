6f217491b5afb2523056079812a09e34a8ed416c
varshithchowdary <varshithlion@gmail.com>
2024-11-11 13:33:24 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -9,8 +9,7 @@ def decode_message(s: str, p: str) -> bool:
             dp[i][0] = dp[i - 1][0]
         else:
             break  # Once we hit a non-* character, we stop filling this row
-    
-    # Fill the DP table
+
     for i in range(1, len(p) + 1):
         for j in range(1, len(s) + 1):
             if p[i - 1] == '*':
