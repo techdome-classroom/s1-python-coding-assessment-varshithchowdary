2bca741dfddd5a6da4df85d8bd3dd7d66c01301d
varshithchowdary <varshithlion@gmail.com>
2024-11-11 13:33:19 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -14,7 +14,7 @@ def decode_message(s: str, p: str) -> bool:
     for i in range(1, len(p) + 1):
         for j in range(1, len(s) + 1):
             if p[i - 1] == '*':
-                # '*' can match zero characters (dp[i-1][j]) or one or more characters (dp[i][j-1])
+
                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
             elif p[i - 1] == '?' or p[i - 1] == s[j - 1]:
 
